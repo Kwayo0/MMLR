07b882dc44530ce5ecc705578153bdb25f119443
Kwayo0 <151820199+Kwayo0@users.noreply.github.com>
2026-02-26 00:36:00 -0600

    I will no longer be providing the project folder

--- mmlr/scenes/range_plus_1.gd
+++
@@ -1,9 +0,0 @@
-extends MeshInstance3D
-
-func _on_static_body_3d_area_entered(area: Area3D) -> void:
-	for i in get_parent().get_node("Projectiles").get_children():
-		if area == i:
-			var e = get_parent().get_node("Megaman")
-			e.bRange += 1
-			if e.bRange > 7:
-				e.bRange = 7
